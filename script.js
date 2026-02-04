2b64d9ea6dc4ace46303b1ec7a0515e8709bee5b
Ismael Morais <ismaelmorais691@gmail.com>
2026-02-03 21:25:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirWhatsApp() {
+    window.location.href = "https://wa.me/5589999869439";
+}
+
+function abrirInstagram() {
+    window.location.href = "https://www.instagram.com/ismaelmr2025";
+}
